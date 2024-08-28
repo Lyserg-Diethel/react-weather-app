a0b2d40385aaa82e6489a3fe04c380cae7b59807
Lyserg-Diethel <Lyserg_Diethel@protonmail.com>
2024-08-28 21:54:19 +0300

    Added functionality to resolve same-name cities within the same country

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import './App.css'
 function App() {
   const [citySelected, setCitySelected] = useState(false);
   const [cityWeatherData, setCityWeatherData] = useState({})
+  const [shouldShowDisplay, setShouldShowDisplay] = useState([]);
   const [hourlyData, setHourlyData] = useState(
     {
       minTemp: null,
@@ -21,25 +22,28 @@ function App() {
       maxWindSpeed: null,
       temperature_2m: null
     }
-  ); 
+  );
+
   return (
     <>
       <div>
       </div>
       <div className="card">
-       <InputForm
+        <InputForm
           locations={locations}
           setCitySelected={setCitySelected}
           setHourlyData={setHourlyData}
           cityWeatherData={cityWeatherData}
           setCityWeatherData={setCityWeatherData}
-          />
-       {
-        citySelected &&
-        <DisplayArea 
-          locations={locations}
-          hourlyData={hourlyData}
-          cityWeatherData={cityWeatherData}
+          shouldShowDisplay={shouldShowDisplay}
+          setShouldShowDisplay={setShouldShowDisplay}
+        />
+        {
+          citySelected && shouldShowDisplay &&
+          <DisplayArea
+            locations={locations}
+            hourlyData={hourlyData}
+            cityWeatherData={cityWeatherData}
           />
         }
       </div>
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,33 +1,62 @@
 import React from 'react';
-import {useState} from 'react';
-import weatherCodes from '../data/weather_codes';
+import { useState, useEffect } from 'react';
 
 function InputForm(props) {
-    const [cityInputValue, setCityInputValue] = useState("")
-    const [countryInputValue, setCountryInputValue] = useState("")
-    const [weatherCode, setWeatherCode] = useState(null)
-    
+  const [cityInputValue, setCityInputValue] = useState("")
+  const [countryInputValue, setCountryInputValue] = useState("")
+  const [sameNameCitiesFound, setSameNameCitiesFound] = useState(null);
+  const [selectedAdminRegion, setSelectedAdminRegion] = useState(null);
+  const [duplicateCityOptions, setDuplicateCityOptions] = useState([]);
 
-    function changeCityInputValue(event) {
-      setCityInputValue(event.target.value);
-    }
-  
-    function changeCountryInputValue(event) {
-      setCountryInputValue(event.target.value);
-    }
+  let duplicateCityOptionsTemp = [];
+
+  useEffect(function () {
+    return () => { setSameNameCitiesFound(false); }
+  }, [selectedAdminRegion]);
+
+  function changeCityInputValue(event) {
+    setCityInputValue(event.target.value);
+  }
+
+  function changeCountryInputValue(event) {
+    setCountryInputValue(event.target.value);
+  }
+
+  function resolveAmbiguousCityChoice(event) {
+    setSelectedAdminRegion(event.target.value);
+    getCityData();
+  }
+
+  function getCityDataAndCleanUp() {
+    getCityData();
+    setSelectedAdminRegion(null);
+  }
 
   async function getCityData() {
+    let city;
     const cityInputValueLowercase = cityInputValue.trim().toLowerCase();
     const countryInputValueLowercase = countryInputValue.trim().toLowerCase();
 
-    const city = props.locations.find(
-      cityObject => 
+    const cityArray = props.locations.filter(
+      cityObject =>
         cityObject.city.toLowerCase() === cityInputValueLowercase &&
-        cityObject.country.toLowerCase() === countryInputValueLowercase
-      );
+        cityObject.country.toLowerCase() === countryInputValueLowercase &&
+        (selectedAdminRegion === null || selectedAdminRegion === cityObject.admin_name)
+    );
+    if (cityArray.length <= 1) {
+      city = cityArray[0]
+      fetchWeatherDataForCity(city)
+      props.setShouldShowDisplay(true)
+    } else {
+      setSameNameCitiesFound(true);
+      props.setShouldShowDisplay(false)
 
-    // setCityData(city);
-    fetchWeatherDataForCity(city)
+      cityArray.forEach((option, index) => {
+        duplicateCityOptionsTemp.push(<option value={option.admin_name} key={`select-option-${index}`}>{option.admin_name}</option>)
+      });
+
+      setDuplicateCityOptions(duplicateCityOptionsTemp);
+    }
   }
 
 
@@ -39,47 +68,59 @@ function InputForm(props) {
     let latitude = city.lat;
     let longitude = city.lng;
     let url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=auto`
-        fetch(url)
-          .then(res=>res.json())
-          .then(res=>{
-              const minTemp = Math.min(...res.hourly.temperature_2m)
-              const maxTemp = Math.max(...res.hourly.temperature_2m);
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        const minTemp = Math.min(...res.hourly.temperature_2m)
+        const maxTemp = Math.max(...res.hourly.temperature_2m);
 
-              props.setCityWeatherData(res);
-              props.setHourlyData(
-                {
-                  minTemp: minTemp,
-                  maxTemp: maxTemp,
-                  tempRange: minTemp < 0 ? maxTemp - minTemp : maxTemp,
-                  minWindSpeed: Math.min(...res.hourly.wind_speed_10m),
-                  maxWindSpeed: Math.max(...res.hourly.wind_speed_10m),
-                  temperature_2m: res.hourly.temperature_2m
-                }
-              )
-              props.setCitySelected(true);
-              if(res.current) {
-                setWeatherCode(weatherCodes[res.current.weather_code])
-              }
+        props.setCityWeatherData(res);
+        props.setHourlyData(
+          {
+            minTemp: minTemp,
+            maxTemp: maxTemp,
+            tempRange: minTemp < 0 ? maxTemp - minTemp : maxTemp,
+            minWindSpeed: Math.min(...res.hourly.wind_speed_10m),
+            maxWindSpeed: Math.max(...res.hourly.wind_speed_10m),
+            temperature_2m: res.hourly.temperature_2m
+          }
+        )
+        props.setCitySelected(true);
 
-              return res;
-          })
+        return res;
+      })
   }
 
-    return (
-        <div className="inputForm">
-            <div className="inputsContainer">
-              <div className="inputWrapper">
-                <label htmlFor="cityInput">City:</label>
-                <input id="cityInput" type="text" placeholder='City' value={cityInputValue} onChange={changeCityInputValue}/>
-              </div>
-              <div className="inputWrapper">
-                <label htmlFor="countryInput">Country:</label>
-                <input id="countryInput" type="text" placeholder='Country' value={countryInputValue} onChange={changeCountryInputValue}/>
-              </div>
+  return (
+    <>
+      <div className="inputForm">
+        <div className="inputsContainer">
+          <div className="inputWrapper">
+            <label htmlFor="cityInput">City:</label>
+            <input id="cityInput" type="text" placeholder='City' value={cityInputValue} onChange={changeCityInputValue} />
+          </div>
+          <div className="inputWrapper">
+            <label htmlFor="countryInput">Country:</label>
+            <input id="countryInput" type="text" placeholder='Country' value={countryInputValue} onChange={changeCountryInputValue} />
+          </div>
+
+          {sameNameCitiesFound ?
+            <div className="inputWrapper">
+              <label htmlFor="Region">Region:</label>
+              <select name="Region" onChange={resolveAmbiguousCityChoice}>
+                <option key="select-option">Select</option>
+                {duplicateCityOptions}
+              </select>
             </div>
-            <button type="button" onClick={getCityData}>Get weather data</button>
+            :
+            <div className="inputWrapper">
+            </div>
+          }
         </div>
-    )
+        <button type="button" onClick={getCityDataAndCleanUp}>Get weather data</button>
+      </div>
+    </>
+  )
 }
 
 export default InputForm;
